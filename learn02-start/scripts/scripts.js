fda5de8dc6fb7e4e76dc3a0d24a48ae6f593e757
Josh LaSota <jlasota126@students.mchenry.edu>
2024-01-17 11:56:54 -0600

    Initial Commit
    new file:   learn-git/index.html
    new file:   learn02-start/css/styles.css
    new file:   learn02-start/images/cat.jpg
    new file:   learn02-start/images/dog.jpg
    new file:   learn02-start/index.html
    new file:   learn02-start/scripts/scripts.js

---
+++ learn02-start/scripts/scripts.js
@@ -0,0 +1,11 @@
+function myFunction2(){
+    document.getElementById("sample").innerHTML=
+    "Changed from a function in the head section";
+}
+
+// single line comment
+
+/*
+Block comment
+*/
+
